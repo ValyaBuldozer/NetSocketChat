54a34210aa0a928d608b6617f496b824f9d56721
Владислав Котельников <kiselek44@gmail.com>
2017-11-30 17:14:09 +0300

    go

---
+++ WebServer/WebServer/ChatHandler.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServer
+{
+    /// <summary>
+    /// Сводное описание для ChatHandler
+    /// </summary>
+    public class ChatHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Привет всем!");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
